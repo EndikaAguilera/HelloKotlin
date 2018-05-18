35fd07eb367a61fad927e9e9e2d9cbbe2b133988
thisobeystudio <thisobeystudio@gmail.com>
2018-05-18 20:06:27 +0200

    more spices options added

--- src/Spice.kt
+++ src/Spice.kt
@@ -1,7 +1,46 @@
 fun main(args: Array<String>) {
+
     val simpleSpice = SimpleSpice()
     println("${simpleSpice.name} ${simpleSpice.spiciness} ${simpleSpice.heat}")
+
+    makeSalt()
 }
+
+class Spice(val name: String, val spiciness: String = "mild") {
+
+    val heat: Int
+        get() {
+            return when (spiciness) {
+                "mild" -> 1
+                "medium" -> 3
+                "spicy" -> 5
+                "very spicy" -> 7
+                "extremely spicy" -> 10
+                else -> 0
+            }
+        }
+
+    init {
+        println("${this.name} ${this.spiciness}")
+    }
+
+}
+
+val spices1 = listOf(
+        Spice("curry", "mild"),
+        Spice("pepper", "medium"),
+        Spice("cayenne", "spicy"),
+        Spice("ginger", "mild"),
+        Spice("red curry", "medium"),
+        Spice("green curry", "mild"),
+        Spice("hot pepper", "extremely spicy")
+)
+
+val spice = Spice("cayenne", spiciness = "spicy")
+
+val spicelist = spices1.filter { it.heat < 5 }
+
+fun makeSalt() = Spice("Salt")
 
 class SimpleSpice {
 
