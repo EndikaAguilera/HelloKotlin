0f349a07cdfd85645c6fcb2e1bd76b0f9a1e52e4
thisobeystudio <thisobeystudio@gmail.com>
2018-05-18 20:35:12 +0200

    Book.kt created and implemented

---
+++ src/Book.kt
@@ -0,0 +1,18 @@
+open class Book(val title: String, val author: String) {
+
+    open var currentPage = 1
+
+    open fun readPage() {
+        currentPage++
+    }
+
+}
+
+class eBook(title: String, author: String, var format: String = "text") : Book(title, author) {
+
+    private var wordsRead = 0
+
+    override fun readPage() {
+        wordsRead += 250
+    }
+}
