656ab862400bb606479b7e7737b02fa8f834c6c4
thisobeystudio <thisobeystudio@gmail.com>
2018-05-18 23:38:49 +0200

    more color added

--- src/Aquarium/AquariumFish.kt
+++ src/Aquarium/AquariumFish.kt
@@ -13,15 +13,40 @@ class Shark : AquariumFish(), FishAction {
     }
 }
 
-class Plecostomus : AquariumFish(), FishAction {
+//class Plecostomus : AquariumFish(), FishAction,
+//        FishColor by GoldColor {
+//
+//    override fun eat() {
+//        println("munch on algae")
+//    }
+//}
 
-    override val color = "gold"
-
-    override fun eat() {
-        println("munch on algae")
-    }
-}
+class Plecostomus(fishColor: FishColor = GoldColor) :
+        FishAction by PrintFishAction("a lot of algae"),
+        FishColor by GoldColor
 
 interface FishAction {
     fun eat()
+}
+
+interface FishColor {
+    val color: String
+}
+
+// Doesn't make sense to create multiple instance of GoldColor since it always return "gold",
+// so it is declared as object instead of class
+object GoldColor : FishColor {
+    override val color: String
+        get() = "gold"
+}
+
+object RedColor : FishColor {
+    override val color: String
+        get() = "red"
+}
+
+class PrintFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
 }
